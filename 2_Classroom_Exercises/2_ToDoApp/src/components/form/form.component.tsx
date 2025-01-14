4687692c94a64502ffef02e0060843a07e7a4e5b
Hajar Alhajeh <hajar.ihab@gmail.com>
2025-01-14 18:51:06 +0200

    edit Task2

--- 2_Classroom_Exercises/2_ToDoApp/src/components/form/form.component.tsx
+++ 2_Classroom_Exercises/2_ToDoApp/src/components/form/form.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ITodoItem } from '../types';
 import './form.css'; 
 interface IProps{
@@ -6,18 +6,28 @@ interface IProps{
 }
 
 const Form = (props:IProps) => {
+  const [error, setError] = useState("");
   const handelSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const title: string = event.currentTarget['task'].value;
     const isUrgent: boolean = event.currentTarget['urgent'].checked;
     if (title.length > 3) {
+
       const newTask: ITodoItem = {
         id: Date.now(),
         title:title,
         isUrgent,
         isDone:false,
       }
+
       props.onSubmit(newTask);
+      setError("");
+
+      event.currentTarget.reset();
+      
+    }
+    else{
+      setError("Task title must be at least 3 characters long.");
     }
 
   }
@@ -25,6 +35,7 @@ const Form = (props:IProps) => {
   return (
     <form onSubmit={handelSubmit} >
       <input type='text' name="task" />
+      {error && <p style={{ color: "red", fontSize: "14px", margin: "5px 0" }}>{error}</p>}
       <div>
         <label htmlFor='urgent'>urgent</label>
         <input type='checkbox' id='urgent' name='urgent' />
